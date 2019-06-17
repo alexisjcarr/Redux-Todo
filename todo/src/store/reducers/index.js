777ccda99e60d7f6ee95731def09556539af78a8
alexisjcarr <acarr59@gmail.com>
2019-06-16 21:03:00 -0500

    Stretch completed

--- todo/src/store/reducers/index.js
+++ todo/src/store/reducers/index.js
@@ -1,36 +1,44 @@
 import * as actionTypes from "../actions";
 
 const initialState = {
-  todos: []
+  todos: JSON.parse(localStorage.getItem('todos')) || [],
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_TODO:
+      const newTodoArr = [
+        ...state.todos,
+        { task: action.newTodo, id: Date.now(), completed: false }
+      ];
+      localStorage.setItem('todos', JSON.stringify(newTodoArr));
       return {
         ...state,
-        todos: [
-          ...state.todos,
-          { task: action.newTodo, id: Date.now(), completed: false }
-        ]
+        todos: newTodoArr
       };
+
     case actionTypes.TOGGLE_TODO:
+      const toggledList = state.todos.map(todo => {
+        if (todo.id === action.id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      });
+      localStorage.setItem('todos', JSON.stringify(toggledList));
       return {
         ...state,
-        todos: state.todos.map(todo => {
-          if (todo.id === action.id) {
-            return {
-              ...todo,
-              completed: !todo.completed
-            }
-          }
-          return todo;
-        })
+        todos: toggledList,
       };
+
     case actionTypes.DELETE_TODO:
+      const filteredList = state.todos.filter(todo => todo.id !== action.id);
+      localStorage.setItem('todos', JSON.stringify(filteredList));
       return {
         ...state,
-        todos: state.todos.filter(todo => todo.id !== action.id)
+        todos: filteredList
       };
     default:
       return state;
